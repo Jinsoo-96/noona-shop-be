0fd39df4f754c5d5c0f8bd9ee1210d68b6205433
신진수 <jins@sinjinsuui-Macmini.local>
2024-10-24 17:26:26 +0900

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+app.use(cors());
+app.use(bodParser.urlencoded({ extended: false }));
+app.use(bodParser.json()); // req.body가 객체로 인식이 됩니다.
+
+const mongoURI = process.env.LOCAL_DB_ADDRESS;
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("mongoose connected"))
+  .catch((err) => console.log("DB connection fail", err));
+
+app.listen(process.env.PORT || 9999, () => console.log("server on"));
