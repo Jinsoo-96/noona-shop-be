20c3526a7a6e7dcefdb77c92ef86436effaca3a8
신진수 <jins@sinjinsuui-Macmini.local>
2024-10-25 17:00:46 +0900

    회원가입 추가 및 env제거

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodParser = require("body-parser");
 const cors = require("cors");
+const indexRouter = require("./routes/index");
 const app = express();
 
 require("dotenv").config();
 app.use(cors());
 app.use(bodParser.urlencoded({ extended: false }));
 app.use(bodParser.json()); // req.body가 객체로 인식이 됩니다.
+
+app.use("/api", indexRouter);
+// /api/user
 
 const mongoURI = process.env.LOCAL_DB_ADDRESS;
 mongoose
