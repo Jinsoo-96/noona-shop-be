02f883294b86a548e41522fbec58780358fe5285
신진수 <jins@sinjinsuui-Macmini.local>
2024-10-26 13:01:38 +0900

    로그인 추가

---
+++ routes/auth.api.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth.controller");
+
+router.post("/login", authController.loginWithEmail);
+
+//get은 body를 보내지 않고, post는 body를 보낸다.
+
+module.exports = router;
