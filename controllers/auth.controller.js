105fa58c89f3254dc9f04db1a2fa71847bf06f38
신진수 <jins@sinjinsuui-Macmini.local>
2024-10-27 19:42:38 +0900

    토큰 로그인

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,9 @@
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const User = require("../models/User");
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
 const authController = {};
 
 authController.loginWithEmail = async (req, res) => {
@@ -20,4 +24,18 @@ authController.loginWithEmail = async (req, res) => {
   }
 };
 
+authController.authenticate = async (req, res, next) => {
+  try {
+    const tokenString = req.headers.authorization;
+    if (!tokenString) throw new Error("Token not found");
+    const token = tokenString.replace("Bearer ", "");
+    jwt.verify(token, JWT_SECRET_KEY, (error, payload) => {
+      if (error) throw new Error("invalid token");
+      req.userId = payload._id; // 토큰 정의할때 _id를 사용했기 때문.
+      next();
+    });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
 module.exports = authController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 //회원가입
 router.post("/", userController.createUser);
+router.get("/me", authController.authenticate, userController.getUser); // 토큰 값을 헤더에 넣을꺼라 get사용 가능
+// 토큰이 valid한 토큰인지, 이 token가지고 유저를 찾아서 리턴
 
 module.exports = router;
